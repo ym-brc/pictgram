bd1349d13964fd21b015a4e490fe3f7d75ddca11
山崎望 <yamazakishirasu@gmail.com>
2021-09-15 11:35:24 +0000

    初期化コミット

---
+++ config/routes.rb
@@ -0,0 +1,19 @@
+Rails.application.routes.draw do
+  get 'users/new'
+  root 'pages#index'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'pages/help'
+  
+end
+
+Rails.application.routes.draw do
+  get 'users/new'
+  root 'pages#index'
+  get 'pages/help'
+end
+
+Rails.application.routes.draw do
+  get 'users/new'
+  root 'pages#index'
+  get 'pages/link'
+end
